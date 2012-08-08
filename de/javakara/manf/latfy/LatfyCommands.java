0811c515b7d3a03a1c849de6e66fb7493b97508e
Björn Heinrichs <muell.mail.d@googlemail.com>
2012-08-09 01:54:34 +0300

    Update de/javakara/manf/latfy/LatfyCommands.java

--- de/javakara/manf/latfy/LatfyCommands.java
+++ de/javakara/manf/latfy/LatfyCommands.java
@@ -1,3 +1,17 @@
+/**************************************************************************
+ * This file is part of MCbb.                                              
+ * MCbb is free software: you can redistribute it and/or modify            
+ * it under the terms of the GNU General Public License as published by    
+ * the Free Software Foundation, either version 3 of the License, or       
+ * (at your option) any later version.                                     
+ * MCbb is distributed in the hope that it will be useful,                 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           
+ * GNU General Public License for more details.                            
+ * You should have received a copy of the GNU General Public License       
+ * along with MCbb.  If not, see <http://www.gnu.org/licenses/>.           
+ *************************************************************************/
+
 package de.javakara.manf.latfy;
 
 import java.util.HashMap;
@@ -48,7 +62,7 @@ public class LatfyCommands implements CommandExecutor {
 	private void broadcast(String name, String[] args) {
 		StringBuilder sb = new StringBuilder();
 		for(String arg:args){
-			sb.append(arg.replace("<text>","").replace("<player>", ""));
+			sb.append(arg.replace("<text>","").replace("<player>", "") + " ");
 		}
 		for(Player p:Bukkit.getOnlinePlayers()){
 			p.sendMessage(ChatColor.translateAlternateColorCodes('&',Config.getString("message").replace("<player>", name).replace("<text>",sb.toString())));
